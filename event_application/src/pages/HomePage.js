9a47ae448c3313ad763208451c003245d9c344fc
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2023-10-01 14:16:49 +0530

    navigation btw pages with links

--- event_application/src/pages/HomePage.js
+++ event_application/src/pages/HomePage.js
@@ -1,5 +1,12 @@
-export default function HomePage(){
+import {Link} from 'react-router-dom';
+
+export default function HomePage() {
     return (
-        <div>This is my homepage !!</div>
+        <>
+            <div>This is my homepage !!</div>
+            <p>
+                Go to <Link to="/products">the list of products</Link>.
+            </p>
+        </>
     )
 }
